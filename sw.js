f8ca600610e32b0b4869955b1fa008005f17681d
it-gittis <165283056+it-gittis@users.noreply.github.com>
2024-04-25 11:37:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,24 @@
-self.addEventListener('install', event => event.waitUntil(
-    caches.open('pwa1').then(cache => cache.add('/'))
-));
+const cacheName = 'password-manager-v1';
+const staticAssets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/images1.png'
+];
 
-self.addEventListener('fetch', event => event.respondWith(
-    caches.open('pwa1')
-    .then(cache => cache.match(event.request))
-    .then(response => response || fetch(event.request))
-));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(staticAssets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
